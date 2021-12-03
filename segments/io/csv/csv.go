3cb62744da1629984b66e2353795cbf35e56db02
Daniel Nägele <git@danieln.de>
2021-12-03 15:52:35 +0100

    give csv segment the ability to limit which fields to export

--- segments/io/csv/csv.go
+++ segments/io/csv/csv.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"reflect"
 	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/bwNetFlow/flowpipeline/segments"
@@ -20,10 +21,11 @@ import (
 type Csv struct {
 	segments.BaseSegment
 	writer     *csv.Writer
-	numFields  int
 	fieldTypes []string
+	fieldNames []string
 
 	FileName string // optional, default is empty which means stdout
+	Fields   string // optional comma-separated list of fields to export, default is "", meaning all fields
 }
 
 func (segment Csv) New(config map[string]string) segments.Segment {
@@ -44,22 +46,39 @@ func (segment Csv) New(config map[string]string) segments.Segment {
 	}
 	newsegment.FileName = filename
 
-	fields := reflect.TypeOf(flow.FlowMessage{})
-	newsegment.numFields = fields.NumField()
-	newsegment.fieldTypes = make([]string, newsegment.numFields)
 	var heading []string
-	for i := 3; i < newsegment.numFields; i++ { // skip over protobuf state, sizeCache and unknownFields
-		field := fields.Field(i)
-		newsegment.fieldTypes[i] = field.Type.String()
-		heading = append(heading, field.Name)
+	if config["fields"] != "" {
+		protofields := reflect.TypeOf(flow.FlowMessage{})
+		conffields := strings.Split(config["fields"], ",")
+		for _, field := range conffields {
+			protofield, found := protofields.FieldByName(field)
+			if !found {
+				log.Printf("[error] Csv: Field specified in 'fields' does not exist.")
+				return nil
+			}
+			newsegment.fieldNames = append(newsegment.fieldNames, field)
+			newsegment.fieldTypes = append(newsegment.fieldTypes, protofield.Type.String())
+			heading = append(heading, field)
+		}
+	} else {
+		protofields := reflect.TypeOf(flow.FlowMessage{})
+		// +-3 skips over protobuf state, sizeCache and unknownFields
+		newsegment.fieldNames = make([]string, protofields.NumField()-3)
+		newsegment.fieldTypes = make([]string, protofields.NumField()-3)
+		for i := 3; i < protofields.NumField(); i++ {
+			field := protofields.Field(i)
+			newsegment.fieldNames[i-3] = field.Name
+			newsegment.fieldTypes[i-3] = field.Type.String()
+			heading = append(heading, field.Name)
+		}
 	}
 
 	newsegment.writer = csv.NewWriter(file)
-	newsegment.writer.Flush()
 	if err := newsegment.writer.Write(heading); err != nil {
 		log.Println("[error] Csv: Failed to write to destination:", err)
 		return nil
 	}
+	newsegment.writer.Flush()
 
 	return newsegment
 }
@@ -73,22 +92,23 @@ func (segment *Csv) Run(wg *sync.WaitGroup) {
 	for msg := range segment.In {
 		var record []string
 		values := reflect.ValueOf(msg).Elem()
-		for i := 3; i < segment.numFields; i++ { // skip over protobuf state, sizeCache and unknownFields
+		for i, fieldname := range segment.fieldNames {
+			protofield := values.FieldByName(fieldname)
 			switch segment.fieldTypes[i] {
 			case "[]uint8": // this is neccessary for proper formatting
-				ipstring := net.IP(values.Field(i).Interface().([]uint8)).String()
+				ipstring := net.IP(protofield.Interface().([]uint8)).String()
 				if ipstring == "<nil>" {
 					ipstring = ""
 				}
 				record = append(record, ipstring)
 			case "uint32": // this is because FormatUint is much faster than Sprint
-				record = append(record, strconv.FormatUint(uint64(values.Field(i).Interface().(uint32)), 10))
+				record = append(record, strconv.FormatUint(uint64(protofield.Interface().(uint32)), 10))
 			case "uint64": // this is because FormatUint is much faster than Sprint
-				record = append(record, strconv.FormatUint(uint64(values.Field(i).Interface().(uint64)), 10))
+				record = append(record, strconv.FormatUint(uint64(protofield.Interface().(uint64)), 10))
 			case "string": // this is because doing nothing is also much faster than Sprint
-				record = append(record, values.Field(i).Interface().(string))
+				record = append(record, protofield.Interface().(string))
 			default:
-				record = append(record, fmt.Sprint(values.Field(i)))
+				record = append(record, fmt.Sprint(protofield))
 			}
 		}
 		segment.writer.Write(record)

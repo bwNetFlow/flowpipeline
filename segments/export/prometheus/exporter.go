516239aa7a14270f50d180566dfabcb5fb9303fc
Daniel Nägele <git@danieln.de>
2022-10-24 17:40:03 +0200

    make prometheus segment accept any label sets

--- segments/export/prometheus/exporter.go
+++ segments/export/prometheus/exporter.go
@@ -3,10 +3,8 @@ package prometheus
 import (
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 
-	"github.com/bwNetFlow/flowpipeline/pb"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
@@ -72,60 +70,11 @@ func (e *Exporter) ServeEndpoints(segment *Prometheus) {
 	log.Printf("Enabled Prometheus %s and %s endpoints.", segment.MetricsPath, segment.FlowdataPath)
 }
 
-func (e *Exporter) Increment(flow *pb.EnrichedFlow) {
-	var application string
-	_, appGuess1 := filterPopularPorts(flow.GetSrcPort())
-	_, appGuess2 := filterPopularPorts(flow.GetDstPort())
-	if appGuess1 != "" {
-		application = appGuess1
-	} else if appGuess2 != "" {
-		application = appGuess2
-	}
-
-	peer := flow.Peer()
-	var remoteAS string
-	if flow.GetFlowDirection() == 0 {
-		remoteAS = nameThatAS(flow.GetSrcAS())
-	} else {
-		remoteAS = nameThatAS(flow.GetDstAS())
-	}
-
-	labels := prometheus.Labels{}
-	for _, l := range e.labels {
-		switch l {
-		case "router":
-			labels[l] = net.IP(flow.GetSamplerAddress()).String()
-		case "ipversion":
-			labels[l] = flow.IPVersionString()
-		case "application":
-			labels[l] = application
-		case "protoname":
-			labels[l] = fmt.Sprint(flow.GetProtoName())
-		case "direction":
-			labels[l] = flow.FlowDirectionString()
-		case "peer":
-			labels[l] = peer
-		case "remoteas":
-			labels[l] = remoteAS
-		case "remotecountry":
-			labels[l] = flow.GetRemoteCountry()
-		case "src_port":
-			labels[l] = fmt.Sprint(flow.SrcPort)
-		case "dst_port":
-			labels[l] = fmt.Sprint(flow.DstPort)
-		case "src_addr":
-			labels[l] = fmt.Sprint(flow.SrcAddr)
-		case "dst_addr":
-			labels[l] = fmt.Sprint(flow.DstAddr)
-		default:
-			labels[l] = ""
-		}
-	}
-
+func (e *Exporter) Increment(bytes uint64, packets uint64, labelset prometheus.Labels) {
 	e.kafkaMessageCount.Inc()
 	// e.flowNumber.With(labels).Inc()
 	// flowPackets.With(labels).Add(float64(flow.GetPackets()))
-	e.flowBits.With(labels).Add(float64(flow.GetBytes()) * 8)
+	e.flowBits.With(labelset).Add(float64(bytes) * 8)
 }
 
 func (e *Exporter) IncrementCtrl(topic string, partition int32, offset int64) {
@@ -134,126 +83,4 @@ func (e *Exporter) IncrementCtrl(topic string, partition int32, offset int64) {
 		"partition": fmt.Sprint(partition),
 	}
 	e.kafkaOffsets.With(labels).Add(float64(offset))
-}
-
-func filterPopularPorts(port uint32) (uint32, string) {
-	switch port {
-	case 80:
-		return port, "http"
-	case 443:
-		return port, "https"
-	case 20, 21:
-		return port, "ftp"
-	case 22:
-		return port, "ssh"
-	case 23:
-		return port, "telnet"
-	case 53:
-		return port, "dns"
-	case 25, 465:
-		return port, "smtp"
-	case 110, 995:
-		return port, "pop3"
-	case 143, 993:
-		return port, "imap"
-	}
-	return 0, ""
-}
-
-func nameThatAS(asn uint32) string {
-	asnmap := map[uint32]string{
-		43:     "Brookhaven National Laboratory",
-		70:     "National Library Medicine USA",
-		174:    "Cogent",
-		513:    "CERN",
-		559:    "SWITCH",
-		680:    "DFN",
-		702:    "Verizon",
-		714:    "Apple",
-		786:    "JANET",
-		1239:   "Sprint",
-		1273:   "Vodafone",
-		1297:   "CERN",
-		1299:   "Telia",
-		1754:   "DESY",
-		2018:   "AFRINIC",
-		2603:   "NORDUnet",
-		2906:   "Netflix",
-		2914:   "NTT",
-		3209:   "Vodafone",
-		3257:   "GTT",
-		3303:   "Swisscom",
-		3320:   "Deutsche Telekom",
-		3356:   "CenturyLink",
-		4356:   "Epic Games",
-		5430:   "freenet",
-		5501:   "Fraunhofer",
-		5511:   "Orange",
-		6185:   "Apple",
-		6453:   "TATA",
-		6507:   "Riot Games",
-		6724:   "Strato",
-		6735:   "sdt.net",
-		6805:   "Telefonica",
-		6830:   "Vodafone",
-		6939:   "Hurricane Electric",
-		7018:   "AT&T",
-		8068:   "Microsoft",
-		8075:   "Microsoft",
-		8220:   "Colt",
-		8403:   "Spotify",
-		8560:   "1&1",
-		8674:   "Netnod",
-		8763:   "DENIC",
-		8881:   "Versatel",
-		9009:   "GLOBALAXS",
-		10310:  "Yahoo",
-		13030:  "Init7",
-		13335:  "Cloudflare",
-		15133:  "Verizon",
-		15169:  "Google",
-		16276:  "OVH",
-		16509:  "Amazon",
-		16591:  "Google Fiber",
-		16625:  "Akamai",
-		19679:  "Dropbox",
-		20446:  "Highwinds",
-		20504:  "RTL",
-		20677:  "imos",
-		20940:  "Akamai",
-		22822:  "Limelight",
-		24429:  "Alibaba",
-		24940:  "Hetzner",
-		30361:  "Swiftwill",
-		31334:  "Kabel Deutschland",
-		32590:  "Valve Steam",
-		32934:  "Facebook",
-		33915:  "Vodafone",
-		35402:  "ecotel",
-		36459:  "Github",
-		36561:  "Google",
-		37963:  "Alibaba",
-		39702:  "S-IT",
-		41552:  "Ebay",
-		41690:  "Dailymotion",
-		46489:  "Twitch",
-		48918:  "Globalways",
-		54113:  "Fastly",
-		54994:  "QUANTIL",
-		57976:  "Blizzard",
-		58069:  "KIT",
-		60781:  "Leaseweb",
-		61339:  "LHC",
-		197540: "Netcup",
-		197602: "TV-9",
-		206339: "Schuler Pressen",
-	}
-	if name, ok := asnmap[asn]; ok {
-		return name
-	} else {
-		if asn == 0 {
-			return "unset"
-		}
-		return "other"
-	}
 }
